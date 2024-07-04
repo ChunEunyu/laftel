ed929d8b2303dfd4ce59bcd095ffd474643799a0
ChunEunyu <xhoneyx01@naver.com>
2024-07-04 21:33:48 +0900

    Feat: 각 요일 별 신작 데이터 get

--- server/routes/daily.js
+++ server/routes/daily.js
@@ -14,66 +14,53 @@ router.get("/", async (req, res) => {
 });
 
 // 월요일 신작 데이터 불러오기
-router.get("/mon", async (req, res) => {
+router.get("/monday", async (req, res) => {
     let collection = await db.collection("daily");
     let results = await collection.find({ "distributed_air_time" : "월요일" }).toArray();
     res.send(results).status(200);
 });
 
 // 화요일 신작 데이터 불러오기
-router.get("/tue", async (req, res) => {
+router.get("/tuesday", async (req, res) => {
     let collection = await db.collection("daily");
     let results = await collection.find({ "distributed_air_time" : "화요일" }).toArray();
     res.send(results).status(200);
 });
 
 // 수요일 신작 데이터 불러오기
-router.get("/wed", async (req, res) => {
+router.get("/wednesday", async (req, res) => {
     let collection = await db.collection("daily");
     let results = await collection.find({ "distributed_air_time" : "수요일" }).toArray();
     res.send(results).status(200);
 });
 
 // 목요일 신작 데이터 불러오기
-router.get("/tue", async (req, res) => {
+router.get("/thursday", async (req, res) => {
     let collection = await db.collection("daily");
     let results = await collection.find({ "distributed_air_time" : "목요일" }).toArray();
     res.send(results).status(200);
 });
 
-// 화요일 신작 데이터 불러오기
-router.get("/tue", async (req, res) => {
+// 금요일 신작 데이터 불러오기
+router.get("/friday", async (req, res) => {
     let collection = await db.collection("daily");
-    let results = await collection.find({ "distributed_air_time" : "화요일" }).toArray();
+    let results = await collection.find({ "distributed_air_time" : "금요일" }).toArray();
     res.send(results).status(200);
 });
 
-// 화요일 신작 데이터 불러오기
-router.get("/tue", async (req, res) => {
+// 토요일 신작 데이터 불러오기
+router.get("/saturday", async (req, res) => {
     let collection = await db.collection("daily");
-    let results = await collection.find({ "distributed_air_time" : "화요일" }).toArray();
+    let results = await collection.find({ "distributed_air_time" : "토요일" }).toArray();
     res.send(results).status(200);
 });
 
-// 화요일 신작 데이터 불러오기
-router.get("/tue", async (req, res) => {
+// 일요일 신작 데이터 불러오기
+router.get("/sunday", async (req, res) => {
     let collection = await db.collection("daily");
-    let results = await collection.find({ "distributed_air_time" : "화요일" }).toArray();
+    let results = await collection.find({ "distributed_air_time" : "일요일" }).toArray();
     res.send(results).status(200);
 });
 
-// 화요일 신작 데이터 불러오기
-router.get("/tue", async (req, res) => {
-    let collection = await db.collection("daily");
-    let results = await collection.find({ "distributed_air_time" : "화요일" }).toArray();
-    res.send(results).status(200);
-});
-
-// 화요일 신작 데이터 불러오기
-router.get("/tue", async (req, res) => {
-    let collection = await db.collection("daily");
-    let results = await collection.find({ "distributed_air_time" : "화요일" }).toArray();
-    res.send(results).status(200);
-});
 
 export default router;
