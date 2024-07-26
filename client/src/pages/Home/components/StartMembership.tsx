89d375c0883bcd98c492c027df32ebf740bffdf8
ChunEunyu <xhoneyx01@naver.com>
2024-07-26 22:16:49 +0900

    feat: 스타일링 수정 및 링크 연결

---
+++ client/src/pages/Home/components/StartMembership.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import StartMembershipLogo from '@/assets/StartMembershipLogo';
+import { Link } from 'react-router-dom';
+
+const StartMembership = () => {
+  return (
+    <Link to='/membership' className='w-full mt-5 mb-5 max-lg:h-16 lg:h-20 lg:pt-1 lg:pb-1 max-lg:pt-3 max-lg:pb-3 rounded-sm bg-light-purple flex flex-col justify-center items-center'>
+      <StartMembershipLogo />
+    </Link>
+  );
+}
+
+export default StartMembership;
+
