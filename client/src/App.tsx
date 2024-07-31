b78d782b30a147a9910d32937610a3cc84e486b5
ChunEunyu <xhoneyx01@naver.com>
2024-07-31 23:18:17 +0900

    feat 애니메이션 모달을 전역적으로 띄어주기

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,28 @@
-import { Outlet } from 'react-router-dom'
-import Header from './components/Header'
+import { Outlet, useSearchParams } from 'react-router-dom';
+import Header from './components/Header';
+import { useAppSelector } from './app/hooks';
+import { selectId, selectToggle } from './features/modal/animeModalSlice';
+import AnimeModal from './pages/AnimeModal';
+import { useEffect } from 'react';
 
 function App() {
+  const isOpen = useAppSelector(selectToggle);
+  const id = useAppSelector(selectId);
+  const [addedParams, setAddedParams] = useSearchParams();
+
+  useEffect(() => {
+    if (isOpen) {
+      setAddedParams({ modal: id.toString() });
+    } else {
+      setAddedParams({});
+    }
+  }, [isOpen, id])
+
   return (
     <>
       <Header />
       <Outlet />
+      { isOpen && <AnimeModal /> }
     </>
   )
 }
