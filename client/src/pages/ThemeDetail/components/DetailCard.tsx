be61bd266ed87b362dcf80e89a1d2a25ac76facb
ChunEunyu <xhoneyx01@naver.com>
2024-08-01 23:18:11 +0900

    fix: 테마추천 페이지 내 디테일 페이지 애니메이션 모달 클릭 이벤트 추가 및 데이터 연결

--- client/src/pages/ThemeDetail/components/DetailCard.tsx
+++ client/src/pages/ThemeDetail/components/DetailCard.tsx
@@ -1,31 +1,54 @@
+import { useAppDispatch } from "@/app/hooks";
 import { FaStar } from "react-icons/fa";
+import { ThemeItem } from "@/types/recommends-themes";
+import { toggle, animeId } from "@/features/modal/animeModalSlice";
 
-const DetailCard = () => {
+const DetailCard = ({ data }: { data: ThemeItem }) => {
+  const dispatch = useAppDispatch();
+  const anime = data.item;
+  const img = anime.images[1] ? anime.images[1].img_url : anime.images[0].img_url;
+
+  const handleClick = () => {
+    dispatch(toggle());
+    dispatch(animeId(anime.id));
+  }
+
   return (
-    <div className='flex items-start p-6 cursor-pointer gap-6'>
+    <div className='flex items-start p-6 cursor-pointer gap-6' onClick={handleClick}>
         <img 
-            className='flex-shrink-0 p-0 lg:w-[22.5rem] lg:h-48 max-lg:w-[13rem] max-lg:h-32 object-cover rounded-sm'
-            src='https://image.laftel.net/items/thumbs/big/dc55e1de-0ade-482d-bceb-aba6bc776673.jpg' 
+            className='flex-shrink-0 p-0 lg:w-[22.5rem] lg:h-48 max-lg:w-[13rem] max-lg:h-32 object-cover rounded-md'
+            src={img}
             alt='' 
         />
-        <div className='flex flex-col shrink'>
-            <p className='text-[1.4rem] font-bold'>귀멸의 칼날</p>
+        <div className='flex flex-col shrink w-full'>
+            <p className='text-[1.4rem] font-bold'>{anime.name}</p>
             <div className='flex pt-1 gap-2 text-smoked-black'>
                 <div className="flex flex-row gap-1">
                     <FaStar className="text-purple text-xl" />
-                    <p className="text-purple font-bold">4.5</p>
+                    <p className="text-purple font-bold">
+                        {anime.avg_rating.toFixed(1)}
+                    </p>
                 </div>
                 <div className="flex flex-row gap-1">
-                    <p>판타지</p>
+                    <p>
+                        {anime.genre.length > 1 ? 
+                            anime.genre.slice(0, 2).join('·') : 
+                            anime.genre.join()
+                        }
+                    </p>
                     <p>|</p>
-                    <p>TVA·성인·완결</p>
+                    <p>{anime.medium}</p>
+                    <p>·</p>
+                    <p>{ anime.content_rating.includes('성인')? 
+                            anime.content_rating.slice(0,2) : anime.content_rating.slice(0,3)
+                        }
+                    </p>
+                    <p>·</p>
+                    { anime.is_ending === true? <p>완결</p> : <p>방영중</p> }
                 </div>
             </div>
             <p className="pt-7">
-                여러분들, 성진우씨 기운 좀 팍팍 받아가시죠. 렙업 ㄱㄱ
-                <br />
-                <br />
-                다른 차원과 이쪽 세계를 이어주는 통로인 '게이트'가 갑자기 발생한 뒤 10년. 세계에는 '헌터'라고 불리는 초인적인 힘에 각성한 사람들이 출현했다. 헌터는 게이트 안의 던전을 공략하고 대가를 받는 것을 생업으로 하고 있는데 그런 강자들만 가득한 헌터들 중 '성진우'는 인류 최약병기라고 불리는 저랭크 헌터로서 생활하고 있었다. 어느 날, 저랭크 던전에 숨겨진 고랭크 이중 던전을 발견했지만 빈사의 중상을 입은 성진우의 눈앞에 의문의 퀘스트창이 나타났는데. 죽음의 순간, 퀘스트를 수락하기로 결심한 성진우는 자신만 '레벨업'할 수 있게 되었는데..!
+                {data.ment}
             </p>
         </div>
     </div>
--- client/src/pages/ThemeDetail/index.tsx
+++ client/src/pages/ThemeDetail/index.tsx
@@ -1,25 +1,53 @@
 import React from 'react';
 import DetailCard from './components/DetailCard';
+import { useLoaderData } from 'react-router-dom';
+import { Themes } from '@/types/recommends-themes';
 
 export default function ThemeDetail() {
+  const { themesById: data } = useLoaderData() as { themesById : Themes };
+  
   return (
     <div className="min-h-screen flex flex-col">
       <div className='w-full bg-smoked-black h-[283px]'>
-        <div className='flex flex-col lg:mx-auto justify-center items-start max-lg:px-6 h-full w-[calc(70vw)] max-w-[1180px]'>
+        <div 
+          className='absolute top-16 right-0 w-[630px] max-w-full h-[283px] aspect-[780/440] object-cover'
+          style={{
+            background: `
+              linear-gradient(
+                to left,
+                rgba(80, 80, 80, 0) 10%,
+                rgba(80, 80, 80, 0) 25%,
+                rgba(80, 80, 80, 0) 50%,
+                rgba(80, 80, 80, 0.5) 75%,
+                rgba(80, 80, 80, 1) 100%
+              ),
+              linear-gradient(
+                to bottom,
+                rgba(25, 27, 42, 0) 10%,
+                rgba(25, 27, 42, 0) 25%,
+                rgba(25, 27, 42, 0) 50%,
+                rgba(25, 27, 42, 0.25) 75%,
+                rgba(25, 27, 42, 1) 100%
+              ),
+              url(${data.theme_item_list[1].item.images[1].img_url})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'right center',
+            opacity: 0.5,
+          }}
+        />
+        <div className='relative flex flex-col lg:mx-auto justify-center items-start max-lg:px-6 h-full w-[calc(70vw)] max-w-[1180px]'>
           <p className='text-white text-3xl font-bold mb-4'>
-            2024년에도 라프텔과 함께
+            {data.title}
           </p>
           <p className='text-white font-semibold'>
-            푸른 용의 기운을 이번 분기 신작으로 받아가시고 즐거운 설 연휴 보내세요~
+            {data.content}
           </p>
         </div>
       </div>
       <div className='max-w-[1232px] w-full my-4 mx-auto mt-4 mr-auto mb-4 ml-auto'>
-        <DetailCard />
-        <DetailCard />
-        <DetailCard />
-        <DetailCard />
-        <DetailCard />
+        {data.theme_item_list.map((item) => (
+          <DetailCard data={item} />
+        ))}
       </div>
     </div>
   );
