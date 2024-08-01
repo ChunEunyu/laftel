64f883ad3ad83b4b9fe325f5720e1c31a9c41765
ChunEunyu <xhoneyx01@naver.com>
2024-08-01 23:19:54 +0900

    fix: 테마추천 페이지 내 데이터 연결

--- client/src/pages/ThemesAll/components/ThemesCard.tsx
+++ client/src/pages/ThemesAll/components/ThemesCard.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Themes } from '@/types/recommends-themes';
 
-const ThemesCard = () => {
+const ThemesCard = ({ data }: { data: Themes }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/themes/${data.id}`);
+  };
+
   return (
-    <div>
+    <div onClick={handleClick}>
         <img 
             className='w-full object-cover lg:h-56 max-lg:h-36 rounded-sm'
-            src="https://image.laftel.net/items/thumbs/big/dc55e1de-0ade-482d-bceb-aba6bc776673.jpg" 
+            src={data.theme_item_list[1].item.images[0].img_url}
             alt='' 
         />
         <p className='text-lg font-bold mb-1 truncate'>
-            2024년에도 라프텔과 함께
+            {data.title}
         </p>
         <p className="text-sm font-normal m-0 " >
-            푸른 용의 기운을 이번 분기 신작으로 받아가시고 즐거운 설 연휴 보내세요~
+            {data.content}
         </p>
     </div>
   );
--- client/src/pages/ThemesAll/index.tsx
+++ client/src/pages/ThemesAll/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ThemesCard from './components/ThemesCard';
+import { useLoaderData } from 'react-router-dom';
+import { Themes as ThemesAllProps } from '@/types/recommends-themes';
 
 export default function ThemesAll() {
+  const { themesAll } = useLoaderData() as { themesAll: ThemesAllProps[] };
+  
   return (
     <div className='w-full max-w-[1232px] px-6 mx-auto'>
       <p className='text-2xl font-semibold mt-3 mb-3'>테마 추천</p>
       <div className='grid grid-cols-3 gap-x-4 gap-y-8 mb-12'>
-        <ThemesCard />
-        <ThemesCard />
-        <ThemesCard />
-        <ThemesCard />
+        {themesAll.map((item) => {
+          return (
+            <ThemesCard key={item.id} data={item} />
+          )
+        })}
       </div>
     </div>
   );
