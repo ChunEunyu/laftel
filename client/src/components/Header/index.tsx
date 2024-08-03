89bcced654580a66dd62aeca307c13a6343c0399
ChunEunyu <xhoneyx01@naver.com>
2024-08-04 00:10:30 +0900

    refactor: exclude path 부분 정규 표현식으로 리팩토링

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -11,35 +11,44 @@ const Header = () => {
   const isScroll = useScroll();
 
   const isHome = location.pathname === '/';
-  
-  if (location.pathname.includes('profile') || location.pathname.includes('auth')) {
+  const isMembership = location.pathname === '/membership';
+  const isExcludedPath = /(profile|auth).*/.test(location.pathname);
+
+  if (isExcludedPath) {
     return null; 
   }
   
   const homeClasses = clsx(baseClasses, { [additionalClasses]: isScroll });
-  const pageClasses = clsx(baseClasses, additionalClasses)
+  const pageClasses = clsx(baseClasses, additionalClasses);
+  const membershipClasses = clsx(baseClasses, membershipAdditionalClasses)
 
   return (
     <>
-      {
-        isHome ? (
-          <div className={homeClasses}>
-            <NavLeftList isScroll={isScroll} />
-            <NavRightList isScroll={isScroll} />
-          </div>
-        ) : (
-          <div className={pageClasses}>
-            <NavLeftList isScroll={true} />
-            <NavRightList isScroll={true} />
-          </div>
-        )
-      }
+      { isHome && (
+        <div className={homeClasses}>
+          <NavLeftList isScroll={isScroll} />
+          <NavRightList isScroll={isScroll} />
+        </div>
+      )}
+      { isMembership && (
+        <div className={membershipClasses}>
+          <NavLeftList isScroll={false} />
+          <NavRightList isScroll={false} />
+        </div>
+      )}
+      { !isHome && !isMembership && (
+        <div className={pageClasses}>
+          <NavLeftList isScroll={true} />
+          <NavRightList isScroll={true} />
+        </div>
+      )}
     </>
   );
 }
 
 const baseClasses = 'fixed w-full z-10 pt-5 pl-12 pr-12 pb-5 max-lg:pl-4 max-lg:pr-4 flex justify-between';
 const additionalClasses = 'bg-white h-16 border-b-[2px] border-border-1';
+const membershipAdditionalClasses = 'bg-black h-16 border-b-[2px] border-border-1';
 
 export default Header;
 
