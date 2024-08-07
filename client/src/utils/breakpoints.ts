e33540465c9b6e2ce4d467b2f52d3dc0500b4674
ChunEunyu <xhoneyx01@naver.com>
2024-08-07 13:45:00 +0900

    feat: 슬라이더 브레이크포인트 파일 분리

---
+++ client/src/utils/breakpoints.ts
@@ -0,0 +1,30 @@
+export const breakpoints= {
+    0: {
+      slidesPerView: 2,
+      spaceBetween: 5,
+    },
+    420: {
+      slidesPerView: 3,
+      spaceBetween: 5,
+    },
+    640: {
+      slidesPerView: 3, // sm
+      spaceBetween: 5,
+    },
+    768: {
+      slidesPerView: 4, // md
+      spaceBetween: 5,
+    },
+    1024: {
+      slidesPerView: 4, // lg
+      spaceBetween: 5,
+    },
+    1280: {
+      slidesPerView: 5, // xl
+      spaceBetween: 5,
+    },
+    1536: {
+      slidesPerView: 6, // 2xl
+      spaceBetween: 5,
+    },
+  }
