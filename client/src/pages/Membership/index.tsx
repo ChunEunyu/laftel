78b58d6a067967901e214895f54abaf0fd6a03c3
ChunEunyu <xhoneyx01@naver.com>
2024-08-19 11:34:06 +0900

    feat: 멤버십 페이지

--- client/src/pages/Membership/index.tsx
+++ client/src/pages/Membership/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 const Membership = () => {
   return (
-    <div>
-      Membership
+    <div className='pt-16'>
+      <Outlet />
     </div>
   );
 }
