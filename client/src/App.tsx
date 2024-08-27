4125ff650a516b1896d1158f7f1752999a15a43a
ChunEunyu <xhoneyx01@naver.com>
2024-08-27 23:13:11 +0900

    fix: 모달에 따른 url 쿼리 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Outlet, useSearchParams } from 'react-router-dom';
 import Header from './components/Header';
-import { useAppSelector } from './app/hooks';
-import { selectId, selectToggle } from './features/modal/animeModalSlice';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { selectToggle, setToggle, selectId } from './features/modal/animeModalSlice';
 import AnimeModal from './pages/AnimeModal';
 import { useEffect } from 'react';
 
@@ -9,15 +9,30 @@ function App() {
   const isOpen = useAppSelector(selectToggle);
   const id = useAppSelector(selectId);
   const [addedParams, setAddedParams] = useSearchParams();
+  const dispatch = useAppDispatch();
+  
+  useEffect(() => {
+    const modalId = addedParams.get('modal');
+    
+    if (modalId && !isOpen) {
+      dispatch(setToggle(true));
+
+    } else if (!modalId && isOpen) {
+      dispatch(setToggle(false));
+    
+    }
+  }, [addedParams]);
 
   useEffect(() => {
     if (isOpen) {
       setAddedParams({ modal: id.toString() });
     } else {
-      setAddedParams({});
+      setAddedParams({}, { replace: false });
     }
-  }, [isOpen, id])
+  }, [isOpen, id]);
 
+  
+  
   return (
     <>
       <Header />
