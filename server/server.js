87db8834c515f57c6b2171707b610623dab15ab9
ChunEunyu <xhoneyx01@naver.com>
2024-08-27 23:26:54 +0900

    feat: router 추가 및 쿠키 추가

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 
 import records from "./routes/record.js";
 import carousels from "./routes/carousels.js"
@@ -10,12 +12,20 @@ import search from "./routes/search.js";
 import themes from "./routes/themes.js";
 import info from "./routes/info.js";
 import item from "./routes/item.js";
+import user from "./routes/user.js";
 
-const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(cors());
+const PORT = process.env.PORT || 5050;
+
+app.use(cors({
+  origin: 'http://localhost:5173', // 요청을 허용할 출처
+  credentials: true // 쿠키 및 자격 증명 허용
+}));
 app.use(express.json());
+app.use(cookieParser());
+
+app.use("/user", user)
 app.use("/record", records);
 app.use("/carousels", carousels);
 app.use("/daily", daily);
@@ -25,7 +35,18 @@ app.use("/search", search);
 app.use("/themes", themes);
 app.use("/info", info);
 app.use("/item", item);
-   
+
+const uri = process.env.ATLAS_URI || "";
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  connectTimeoutMS: 30000,  // 30 seconds
+  socketTimeoutMS: 30000    // 30 seconds
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
+
 // start the Express server
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
