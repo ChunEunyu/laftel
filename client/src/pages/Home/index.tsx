777d0dddcc39e08f4099c3acb5ebe63c9e02cd7b
ChunEunyu <xhoneyx01@naver.com>
2024-08-27 23:18:22 +0900

    fix: react query에서 받아올 항목 추가 및 수정

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,35 +1,67 @@
+import { lazy, Suspense } from 'react';
+import { Carousel } from '@/types/carousels';
+import { Themes } from '@/types/recommends-themes';
+import MainCarousels from './components/MainCarousels/MainCarousels';
 import Footer from '@/components/Footer';
-import AnimeSlider from './components/AnimeSlider';
-import MainCarousels from './components/MainCarousels';
-import DayButtonGroup from './components/DayButtonGroup';
-import { useEffect, useState } from 'react';
+import DayButtonGroup from '../../components/DayButtonGroup';
+import StartMembership from './components/StartMembership';
+import AnimeRank from './components/Ranking/AnimeRank';
+import ThemeAnimeSlider from './components/AnimeSlider/ThemeAnimeSlider';
+import MainCarouselSkeleton from './components/MainCarousels/MainCarouselSkeleton';
+import AnimeCardSkeleton from '@/components/Skeleton/AnimeCardSkeleton';
+import { useQuery, UseQueryResult } from '@tanstack/react-query';
+import { getCarousels, getRecommendsThemesBySize } from '@/apis/getAPIs';
+
 
 const Home = () => {
-  const [records, setRecords] = useState([]);
+  const { data: carouselsData, isSuccess, isFetched }: UseQueryResult<Carousel[]> = useQuery({
+    queryKey: ['carousels'],
+    queryFn: () => getCarousels(),
+    staleTime: 60000 * 3,
+  })
 
-  // This method fetches the records from the database.
-  useEffect(() => {
-    async function getRecords() {
-      const response = await fetch(`http://localhost:5050/daily/`);
-      if (!response.ok) {
-        const message = `An error occurred: ${response.statusText}`;
-        console.error(message);
-        return;
-      }
-      const records = await response.json();
-      console.log('records', records);
-      setRecords(records);
-    }
-    getRecords();
-    return;
-  }, [records.length]);
+  const { data: themesBySizeData, isLoading: loadingThemes }: UseQueryResult<Themes[]> = useQuery({
+    queryKey: ['themesBySize'],
+    queryFn: () => getRecommendsThemesBySize(20),
+    staleTime: 60000 * 3,
+  })
 
+  const numbers = Array.from({ length: 19 }, (_, i) => i + 1);
+  
   return (
     <div>
-      <MainCarousels />
+      { isFetched && isSuccess ? (
+        <MainCarousels data={carouselsData} />
+        
+      ) : (
+        <MainCarouselSkeleton />
+      )}
       <div className='pl-8 pr-8 pt-5 pb-5'>
         <DayButtonGroup isHomePage={true} />
-        <AnimeSlider />
+        { !loadingThemes && themesBySizeData ? (
+          <ThemeAnimeSlider data={themesBySizeData[0]} />
+        ) : (
+          <AnimeCardSkeleton />
+        )}
+        <StartMembership />
+        <AnimeRank />
+        { !loadingThemes && themesBySizeData ? (
+          <>
+            { numbers.map((i) => {
+              const themeData = themesBySizeData[i];
+              return (
+                <ThemeAnimeSlider 
+                  key={themeData.id} 
+                  data={themeData} 
+                />
+              );
+            })}
+          </>
+        ) : (
+          <AnimeCardSkeleton />
+        )
+        }
+        
       </div>
       <Footer />
     </div>
