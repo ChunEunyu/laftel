069b8465e5dcd014138ce2eb46370f5ba0a7db18
ChunEunyu <xhoneyx01@naver.com>
2024-08-27 23:19:43 +0900

    fix: 로그인 여부에 따른 라우팅 설정

--- client/src/pages/Membership/index.tsx
+++ client/src/pages/Membership/index.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 const Membership = () => {
+  const auth = sessionStorage.getItem("auth");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!auth) {
+      navigate("/membership/landing");
+    } else {
+      navigate("/membership/subscribed");
+    }
+
+  }, [auth, navigate]);
+  
   return (
     <div className='pt-16'>
       <Outlet />
--- client/src/pages/Profile/index.tsx
+++ client/src/pages/Profile/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import profile1 from '@/assets/profile/profile1.png';
 import { Link } from 'react-router-dom';
 
 export default function Profile() {
+  const isLoggedIn = sessionStorage.getItem('auth');
+  const user = isLoggedIn ? JSON.parse(isLoggedIn) : null;
+  
   return (
     <div className='w-screen h-screen bg-black flex flex-col justify-center items-center'>
       <Link to='/'>
-        <img className='w-60 h-60 rounded-full mb-5' src={profile1} alt='' />
+        <img 
+          className='w-60 h-60 rounded-full mb-5 hover:border-white hover:border-8 transition-all duration-300' 
+          src={user.img} 
+          alt='' 
+        />
       </Link>
-      <p className='text-white text-2xl font-bold'>닉네임</p>
+      <p className='text-white text-2xl font-bold'>{user.name}</p>
     </div>
   );
 }
